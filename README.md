eca12df292d718ff86422725b00a6078a769eef9
int <int>
2019-07-10 13:23:27 -0400

    Readme -/- Comments -/- video or audio check
    -Added redundancy to ensure the extension loads
    -Spotify started randomly using <audio> and <video> interchangeably like absolute madmen so it checks for both now
    -added comments to the code

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
-# spotitySpeedExtension
-change the speed of spotify song playback rate. slow or fast.
+# Spotity Speed Chrome Extension
+Change the speed of spotify song playback rate. slow or fast.
 ![preview img](https://raw.githubusercontent.com/intOrfloat/spotitySpeedExtension/master/promo%20444%20280.png)
 
 Get it here: [Chrome extension link](https://chrome.google.com/webstore/detail/spotify-playback-speed-ac/cgbihpjbhpdfbdckcabcniojdhcgblhd)
+
+
+### Version 1.5:
+ - Added redundancy to ensure the extension loads
+
+ - Spotify started randomly using <audio> and <video> interchangeably like absolute madmen so it checks for both now 
+
+ - added comments to the code 
+
+---
+## Install from github..
++ download project as zip ![preview img](https://i.stack.imgur.com/PrvYK.png)
++ unzip and load the folder as [unpacked extension](https://developer.chrome.com/extensions/getstarted#manifest)
++ remember to click that little refresh button on the extension page for the extension if you make code changes.
+---
+#### ℹ️ The code is in `content-script.js` and is heavily commented 
+---
+## The problem this solves: 
+>  The video/audio element is hidden and only referenced in spotify's encapsulated code
+## This solution: 
++ The code main part of the code is written in a template literal string with back quotes ---> `
+	- This allows a multilined string with double quotes and single quotes without breaking the string variable
+	
++ This string named code is injected into the top of the html dom as a script element when the page loads
+	- It loads before spotify's scripts and can pre-append the browser's code of `document.createElement` before it's used
+	- Whenever spotify's scripts execute `document.createElement('video')` a reference to the element created is stored in VideoElementsMade
+		- `document.createElement('video')` used to be an audio element until spotify started supporting videos  and now it's randomly either
++ The `timeout()` loop is just an added assurance that the playbackspeed input element is created and that the speed is changed to the stored speed from previous sessions
+
+## __[My previous solution(bad)](https://github.com/intOrfloat/SpotifyPlaybackRate)__ 
++ made a full copy of spotify's code 
+     - forced having to block spotify's script with a firewall extension
+     - slow to open with editor and prone to crashing when debugging
+     - firewall made things inconsistent
+     - doesnt ever update when spotify updates :(
++ added `window.spotifyContext = this;` when you click play
+     - this allowed `window.spotifyContext._getTrackPlayer()` from anywhere
 
--- content-script.js
+++ content-script.js
@@ -1,70 +1,94 @@
-var code = `var base = document.createElement;
-var VideoElementsMade = [];
+/* 
+ The problem: the video/audio element is hidden and only referenced in spotify's encapsulated code
+ 
+ 1. The code main part of the code is written in a template literal string with back quotes ``
+ 	- This allows a multilined string with double quotes and single quotes without breaking the string variable
+ 	
+ 2. This string named code is injected into the top of the html dom as a script element when the page loads
+ 	- It loads before spotify's scripts and can pre-append the browser's code of document.createElement before it's used
+ 	- Whenever spotify's scripts execute document.createElement('video') a reference to the element created is stored in VideoElementsMade
+ 		- document.createElement('video') used to be an audio element until spotify started supporting videos and now it's randomly either
+ 3. The timeout is just an added assurance that the playbackspeed input element is created and that the speed is changed to the stored speed from previous sessions
+*/
+/* ======== Start of code string literal ======== */
+var code = `
+	var base = document.createElement; /* A backup reference to the browser's original document.createElement */
+	var VideoElementsMade = []; /* Array of video/audio elements made by spotify's scripts */
+	
+	/* Replacing the DOM's original reference to the browser's createElement function */
+	document.createElement = function(message) {
+		/* base.apply is calling the backup reference of createElement with the arguments sent to our function and assigning it to our variable named element */
+		var element = base.apply(this, arguments); 
+		
+		/* we check the first argument sent to us Examp. document.createElement('video') would have message = 'video' */
+		/* ignores the many document.createElement('div'), document.createElement('nav'), ect... */
+		if(message == 'video' || message == 'audio'){ /* Checking if spotify scripts are making a video or audio element */
+			VideoElementsMade.push(element); /* Add a reference to the element in our array. Arrays hold references not copies by default in javascript. */
+		}
+		return element /* return the element and complete the loop so the page is allowed to be made */
+	};
+	
+	/* When the page is loaded completely... */
+	window.onload = function() {
+		function getStoredSpeed(){ /* Gets stored speed between refreshes*/
+			return localStorage.getItem('speed');
+		}
+		var lastSpeed = getStoredSpeed() || 1.0; /* if stored speed is null make lastSpeed 1.0 */
+	
+		function setStoredSpeed(value){ /* Sets variable in the site's cookie along-side spotify's stuff */
+			localStorage.setItem('speed',value);
+		}
 
-document.createElement = function(message) {
-
-    var element = base.apply(this, arguments);
-    if(message == 'video'){
-        element.playbackRate = 0.5;
-        VideoElementsMade.push(element);
-    }
-    return element
-};
-window.onload = function() {
-
-  function getStoredSpeed(){
-    return localStorage.getItem('speed');
-  }
-  var lastSpeed = getStoredSpeed() || 1.0;
-
-  function setStoredSpeed(value){
-localStorage.setItem('speed',value);
-
-  }
-
-    var input = document.createElement('input');
-    input.type = 'number';
-    input.id = 'speed-extension-input';
-    input.style = 'background-color: #08080859;'
-        + 'border: #823333;'
-        + 'width: 45px;'
-        + 'margin: 5px;';
-    input.value = lastSpeed * 100;
-    input.oninput = function(e){
-        validateAndChangeSpeed();
-    };
-    function validateAndChangeSpeed(value){
-      var val = parseFloat( value || (input.value / 100));
-      if(!isNaN(val)){
-
-          changeSpeed(val);
-      }
-    }
-    function changeSpeed(val) {
-
-      for(var i = 0; i < VideoElementsMade.length; i++){
-       VideoElementsMade[i].playbackRate = val;
-       if(val != lastSpeed){
-         lastSpeed = val;
-         setStoredSpeed(val);
-       }
-      }
-
-    }
-
-    function timeout() {
-        setTimeout(function () {
-          try {
-            validateAndChangeSpeed(lastSpeed);
-          }catch{}
-            timeout();
-        }, 500);
-    }
-    document.getElementsByClassName('now-playing-bar__right')[0].appendChild (input);
-    timeout();
-};`;
-var script = document.createElement('script');
-script.textContent = code;
-document.body.appendChild(script);
-(document.head||document.documentElement).appendChild(script);
-script.remove();
+		/* Building our playback speed input element */
+		var input = document.createElement('input');
+		input.type = 'number';
+		input.id = 'speed-extension-input';
+		input.style = 'background-color: #08080859;'
+			+ 'border: #823333;'
+			+ 'width: 45px;'
+			+ 'margin: 5px;';
+		input.value = lastSpeed * 100;
+		input.oninput = function(e){ /* What happens when we change the number in our input box element */
+			validateAndChangeSpeed();  /* We call our function */
+		};
+		
+		function validateAndChangeSpeed(value){ 
+			var val = parseFloat( value || (input.value / 100)); /* val must be in format 0.0625 - 16.0 https://stackoverflow.com/a/32320020 */
+			if(!isNaN(val)){ /* check if val is a number */
+				changeSpeed(val);
+			}
+		}
+		
+		function changeSpeed(val) {
+			for(var i = 0; i < VideoElementsMade.length; i++){ /* change speed for all elements found (i havent seen this be more than 1 but you never know) */
+				VideoElementsMade[i].playbackRate = val; /* set the playback rate here */
+				if(val != lastSpeed){ /* update the lastSpeed if the speed actually changed */
+					lastSpeed = val;
+					setStoredSpeed(val);
+				}
+			}
+		}
+		
+		function timeout() { /* This function is called by itself over and over */
+			if(document.getElementById('speed-extension-input') == null) /* check if our input element doesnt exist */
+				document.getElementsByClassName('now-playing-bar__right')[0].appendChild (input); /* make our input exist on page */
+				
+			setTimeout(function () { /* setTimeout is a delayed call(500 milliseconds) to the code below */
+				try {
+					validateAndChangeSpeed(lastSpeed); /* this is in a try/catch because if an error happens timeout wouldnt be called again. */
+				}catch{
+					
+				}
+				timeout(); /* call timeout again which starts the loop and eventually it will come back here */
+			}, 500); /* 500ms */
+		}
+		
+		timeout(); /* starts the loop to check and create our inputbox and to set the playback speed without having to mess with input box(by refreshing and having it load from cookie) */
+		/* sometimes playbackRate is set back to 1.0 by spotify's code so timeout just ensures it goes the speed the user desires */
+	};`; 
+/* ======== End of code string literal ======== */
+var script = document.createElement('script'); /* Create our dummy script to be inserted with our code variable  */
+script.textContent = code; /* insert our code as the contents of the script */
+document.body.appendChild(script); /* make our script exist on the page as, hopefully, the first script to execute. */
+(document.head||document.documentElement).appendChild(script); /* appends script again(not good practice) as close to top as possible */
+script.remove(); /* idk why i do this */
